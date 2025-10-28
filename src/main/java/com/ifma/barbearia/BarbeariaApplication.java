0572786ee1c7719742b95da2f58042e0a6fcc9e5
yurepires <contato.yurepires@gmail.com>
2025-10-27 22:58:43 -0300

    fix: AuditAwareImpl

--- src/main/java/com/ifma/barbearia/BarbeariaApplication.java
+++ src/main/java/com/ifma/barbearia/BarbeariaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing(auditorAwareRef = "clienteAuditAwareImpl")
+@EnableJpaAuditing(auditorAwareRef = "auditAware")
 public class BarbeariaApplication {
 
 	public static void main(String[] args) {
