ba0047c065d5bc784db5807dc368d0d75082f212
Davi Cruz <cruzdavi001@gmail.com>
2025-11-07 21:22:26 -0300

    autenticacao de cliente sem senha

---
+++ src/main/java/com/ifma/barbearia/controller/ClienteAuthController.java
@@ -0,0 +1,33 @@
+package com.ifma.barbearia.controller;
+
+import com.ifma.barbearia.DTOs.OtpRequestDto;
+import com.ifma.barbearia.DTOs.OtpValidateDto;
+import com.ifma.barbearia.DTOs.OtpResponseDto;
+import com.ifma.barbearia.services.IClienteOtpService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/cliente", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ClienteAuthController {
+
+    private final IClienteOtpService clienteOtpService;
+
+    public ClienteAuthController(IClienteOtpService clienteOtpService) {
+        this.clienteOtpService = clienteOtpService;
+    }
+
+    @PostMapping("/gerar-Otp")
+    public ResponseEntity<String> gerarOtp(@RequestBody OtpRequestDto request) {
+        clienteOtpService.enviarCodigoPorEmail(request);
+        return ResponseEntity.ok("OTP enviado para o email.");
+    }
+
+    @PostMapping("/validar-Otp")
+    public ResponseEntity<OtpResponseDto> validarOtp(@RequestBody OtpValidateDto validateDto) {
+        OtpResponseDto response = clienteOtpService.validarCodigo(validateDto);
+        return ResponseEntity.ok(response);
+    }
+
+}
